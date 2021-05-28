f088f0e0d7050d17e7d4e5ba7ab43865dab2aff1
isha-naru <84394592+isha-naru@users.noreply.github.com>
2021-05-28 07:22:29 -0700

    Update kaprekarsConstant.js

--- src/2021/may/27/kaprekarsConstant.js
+++ src/2021/may/27/kaprekarsConstant.js
@@ -1,8 +1,27 @@
 // Have the function KaprekarsConstant(num) take the num parameter being passed which will be a 4-digit number with at least two distinct digits. Your program should perform the following routine on the number: Arrange the digits in descending order and in ascending order (adding zeroes to fit it to a 4-digit number), and subtract the smaller number from the bigger number. Then repeat the previous step. Performing this routine will always cause you to reach a fixed number: 6174. Then performing the routine on 6174 will always give you 6174 (7641 - 1467 = 6174). Your program should return the number of times this routine must be performed until 6174 is reached. For example: if num is 3524 your program should return 3 because of the following steps: (1) 5432 - 2345 = 3087, (2) 8730 - 0378 = 8352, (3) 8532 - 2358 = 6174.
+function pad(num){
+  if(num < 10)
+    return '000' + num;
+  if(num < 100)
+    return '00' + num;
+  if (num < 1000)
+    return '0'+num;
 
+    return num;
+}
 function kaprekarsConstant(num) {
-  // code goes here
-  return num;
+  let min = [...pad(num).toString()].sort().join('');
+  let max = [...min].sort().reverse().join('');
+  let inc = 1;
+
+  while((parseInt(max) - parseInt(min) != 6174))
+  {
+    let ans = parseInt(max) - parseInt(min);
+    min = [...pad(ans).toString()].sort().join('');
+    max = [...min].sort().reverse().join('');
+    inc += 1;
+  }
+  return inc;
 }
 
 module.exports = kaprekarsConstant;
