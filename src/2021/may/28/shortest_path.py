ccfa4d7afb907e6d470e04963e72555ffbe6c5ff
isha-naru <84394592+isha-naru@users.noreply.github.com>
2021-05-28 19:53:29 -0700

    Update shortest_path.py
    Some cases are failing! need to edit dictionary

--- src/2021/may/28/shortest_path.py
+++ src/2021/may/28/shortest_path.py
@@ -4,5 +4,71 @@
 # There will only ever be one shortest path for the array. If no path between the first and last node exists, return -1. The array will at minimum have two nodes. Also, the connection A-B for example, means that A can get to B and B can get to A.
 
 def shortest_path(str):
-  # code goes here
-  return str
+  from collections import defaultdict
+def pathCreater(new_path):
+  res = ""
+  for i in range(len(new_path)):
+  res=res+(new_path[i] + " - ")
+  res = res[:1]
+  print(res)
+
+
+def shortest_path(str):
+
+  x  = int(str[0])
+  nodes = []
+  connections = []
+  keys = []
+  values = []
+
+
+  for i in range(0,x):
+   nodes.append(str[i])
+  
+  for i in range(x,len(str)):
+   connections.append(str[i])
+  
+
+  d = defaultdict(set)
+
+  for i in range(len(connections)):
+    partition_string = connections[i].partition('-')
+    key = partition_string[0]
+    value = partition_string[2]
+    keys.append(key)
+    values.append(value)
+  
+
+  for i in range(len(connections)):
+    d[keys[i]].add(values[i])
+
+
+
+
+
+  start = nodes[0]
+  end = nodes[x-1]
+
+  explored = []
+  queue = [[str[1]]]
+  while queue:
+      currPath = queue.pop(0)
+      node = currPath[-1]
+
+      if node not in explored:
+        neighbors = d[node]
+        for neighbor in neighbors:
+          new_path = list(currPath)
+          new_path.append(neighbor)
+          queue.append(new_path)
+
+          if neighbor == end:
+            res=""
+            for i in range(len(new_path)-1):
+              res=res+(new_path[i] + "-")
+            print(res)
+            break
+        explored.append(node)
+  return -1
+
+ 
