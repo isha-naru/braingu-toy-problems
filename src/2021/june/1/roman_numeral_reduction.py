4e3182e5571aba566c32e86840dd570f096fe24f
isha-naru <84394592+isha-naru@users.noreply.github.com>
2021-05-31 00:30:45 -0700

    Update roman_numeral_reduction.py
    Completed

--- src/2021/june/1/roman_numeral_reduction.py
+++ src/2021/june/1/roman_numeral_reduction.py
@@ -2,5 +2,41 @@
 # Your program should return the same number given by str using a smaller set of roman numerals. For example: if str is "LLLXXXVVVV" this is 200, so your program should return CC because this is the shortest way to write 200 using the roman numeral system given above. If a string is given in its shortest form, just return that same string.
 
 def roman_numeral_reduction(str):
-  # code goes here
-  return str
+  roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+  i = 0
+  num = 0
+  while i < len(str):
+    if i+1<len(str) and str[i:i+2] in roman:
+        num+=roman[str[i:i+2]]
+        i+=2
+    else:
+        num+=roman[str[i]]
+        i+=1
+  integer = int(num)
+  val = [
+    1000, 900, 500, 400,
+    100, 90, 50, 40,
+    10, 9, 5, 4,
+    1
+  ]
+  syb = [
+    "M", "CM", "D", "CD",
+    "C", "XC", "L", "XL",
+    "X", "IX", "V", "IV",
+    "I"
+  ]
+  roman_num = ''
+  i = 0
+  while  num > 0:
+    for _ in range(num // val[i]):
+      roman_num += syb[i]
+      num -= val[i]
+    i += 1
+  return roman_num
+
+
+
+
+
+  
+
